6a05635d6f8e4cd0b6478f04311feef1f3d0af6a
waseem-qasim <waseemqasim@gmail.com>
2022-06-24 21:07:00 +0500

    js_task_dice_password_toss

---
+++ app.js
@@ -0,0 +1,73 @@
+// JS TASK 1
+// ===============================================
+
+// var userInput1 = prompt("Enter Your Name");
+// if (userInput1) {
+//   var userToss = prompt("What do want H/T");
+//   if (userToss) {
+//     var number = Math.floor(Math.random() * 2 + 1);
+//     console.log(number);
+
+//     var win;
+
+//     if (number == 1) {
+//       win = "Head";
+//     } else if (number == 2) {
+//       win = "Tail";
+//     } else {
+//       alert("number not correct");
+//     }
+
+//     if (userToss == win) {
+//       alert("Congratz " + userInput1 + " wins as the toss is " + win);
+//     } else {
+//       alert("Sorry " + userInput1 + " lost as the toss is " + win);
+//     }
+//   } else {
+//     alert("input field empty");
+//   }
+// } else {
+//   alert("input field empty");
+// }
+
+
+
+
+// JS TASK 2
+// ===============================================
+
+
+// var alpha = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "A", "b", "c", "C", "d", "D", "E", 'e', "F", 'f', "g",
+// "G", 'H', 'h', 'i', 'I', 'J', 'j', "K", "k", "L", "l", "M",
+// "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s",
+// "T", "t", "U", 'u', "V", 'v', "W", 'w', "x", "X", 'y', "Y", "Z", "z",]
+
+//     var list = [];
+
+//     for (i = 0; i <= 8; i++) {
+//         var number = Math.floor(Math.random() * alpha.length);
+
+//         var word = alpha[number];
+
+//         list.push(word)
+//     }
+//     PassdGen = list.splice(",").join("")
+//     alert (PassdGen)
+
+
+
+
+// JS TASK 3
+// ===============================================
+
+
+
+var number = [ 1, 2, 3, 4, 5, 6,]
+
+var number = Math.floor(Math.random() * 7);
+
+alert (number)
+  
+    
+
+    
